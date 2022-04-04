238f2011a23211b94ad719f4222d4caba4d5e8af
Febrim05 <79125219+Febrim05@users.noreply.github.com>
2022-04-04 16:24:11 +0700

    Add files via upload

---
+++ BilanganPecahan/src/main/java/com/rplbo/pecahan/Fraction.java
@@ -0,0 +1,32 @@
+package com.rplbo.pecahan;
+
+import java.security.PublicKey;
+
+public class Fraction extends Mathematics{
+    public Fraction(int, int){
+    }
+    private int calculateDemoninator(int, int, int){
+
+    }
+    private void checkMultiplesOfNumber(){
+
+    }
+    public Fraction division(Fraction, Fraction, Fraction){
+
+    }
+    public Fraction multiplication(Fraction, Fraction, Fraction){
+
+    }
+    public Fraction substraction(Fraction, Fraction, Fraction){
+
+    }
+    public Fraction summation(Fraction, Fraction, Fraction){
+
+    }
+    public String toString(String convert){
+        int convert1 = Integer.parseInt(convert)
+        this.convert = convert1;
+
+
+    }
+}
---
+++ BilanganPecahan/src/main/java/com/rplbo/pecahan/Mathematics.java
@@ -0,0 +1,39 @@
+package com.rplbo.pecahan;
+
+public class Mathematics {
+    private int numerator;
+    private int denominator;
+
+    public Mathematics(int numerator, int denominator){\
+    }
+    public  division(){
+
+    }
+    private factorize(int, int){
+
+    }
+    public int getDenominator() {
+        return denominator;
+    }
+    public int getNumerator() {
+        return numerator;
+    }
+    public Fraction  multiplications(Fraction, Fraction){
+
+    }
+    public void  setDenominator(int denominator){
+        this.denominator = denominator;
+    }
+    public void setNumerator(int numerator){
+        this. numerator = numerator;
+    }
+    public  Fraction simplifyFaction( int, int){
+
+    }
+    public Fraction subtraction(Fraction, Fraction){
+
+    }
+    public Fraction summation(Fraction, Fraction){
+
+    }
+}
